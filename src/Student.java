6a084baac8fea5a819c041804a769ab0d2ef8e8a
diana <dvaryvod@uwo.ca>
2017-02-15 11:02:24 -0500

    Added Course class, changed Instructor & Student class
    Signed-off-by: diana <dvaryvod@uwo.ca>

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 /**
  * Created by ethan on 2017-02-07.
  */
 public class Student extends Person {
     String major;
+    ArrayList<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
@@ -11,5 +13,14 @@ public class Student extends Person {
 
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
+    }
+
+    public void addCourse(Course c)
+    {
+        if (this.courseList == null)
+        {
+         this.courseList =  new ArrayList<Course>();
+        }
+        this.courseList.add(c);
     }
 }
